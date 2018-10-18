e15dfe16a63c48d62d6ef8b637e30d48e59479a3
pputra <mynameisyoga@live.com>
2018-10-18 18:57:31 +0700

    add board schema

---
+++ game/src/views/Game.vue
@@ -0,0 +1,119 @@
+<template>
+<div class="container">
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+    <div class="row border">
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+        <div class="col-sm border">
+            One of three columns
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'GAME',
+  data () {
+    return {
+      length: 10,
+      width: 3
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
