019994e843179798820f215b036bbb863cb43a1a
harlesbayu <bayuanggaraharles@gmail.com>
2018-10-19 12:33:43 +0700

    room and playes

---
+++ game/config/config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyD1myihk2tsdaYKEy7wXUwRNZpi55yXe7E",
+  authDomain: "doordie-44f5e.firebaseapp.com",
+  databaseURL: "https://doordie-44f5e.firebaseio.com",
+  projectId: "doordie-44f5e",
+  storageBucket: "",
+  messagingSenderId: "998186596819"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.database()
+
+export default db
