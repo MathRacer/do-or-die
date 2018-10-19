9cf915999c05262a463c8d486385e05c7d34de68
harlesbayu <bayuanggaraharles@gmail.com>
2018-10-19 12:33:43 +0700

    room and playes

--- game/src/views/Home.vue
+++ game/src/views/Home.vue
@@ -7,9 +7,10 @@
       <div class="card-body">
         <h5 class="card-title" style="color: #f55052;">CREATE ROOM</h5>
         <div class="form-group">
-          <input type="text" class="form-control" style="color: #f55052;" autofocus>
+          <input type="text" class="form-control" style="color: #f55052;" v-model="room" autofocus>
         </div>
-        <a @click="btnCreateRoom" class="btn btn-primary" style="background:#f55052; color: #ffffff; border: 1px #ffffff solid;"> <i class="fas fa-car"></i>
+        <a @click="btnCreateRoom" class="btn btn-primary" style="background:#f55052; color: #ffffff; border: 1px #ffffff solid;">
+          <i class="fas fa-car"></i>
           LETS GO !!!</a>
       </div>
       <div class="card-footer text-muted"></div>
@@ -18,39 +19,58 @@
 </template>
 
 <script>
+import db from "../../config/config.js"
 
 export default {
   name: 'home',
   components: {},
+  data: function () {
+    return {
+      room: ""
+    }
+  },
   methods: {
     btnCreateRoom: function () {
-      this.$router.push('signin')
+      let self = this
+      localStorage.setItem('room', self.room)
+      if (self.room) {
+        var starCountRef = db.ref('room/' + self.room)
+        starCountRef.once('value', function (snapshot) {
+          if (!snapshot.val()) {
+            db.ref('room/' + self.room).set({
+                name: self.room
+              })
+              .then(response => {
+                self.room = ''
+              })
+          }
+          self.$router.push('signin')
+        })
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+  .home {
+    background-image: url('../assets/room.jpg');
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    min-width: 100%;
+    min-height: 100%;
+  }
 
-.home {
-  background-image: url('../assets/room.jpg');
-  position: fixed; 
-  top: 0; 
-  left: 0; 
-  background-size: cover;
-  min-width: 100%;
-  min-height: 100%;
-}
-
- .boxCreateRoom {
-  width: 500px;
-  /* height: 400px; */
-  opacity: 0.9;
-  top: 50%;
-  left: 50%;
-  position: absolute;
-  margin-top: -200px;
-  margin-left: -250px;
-}
-
+  .boxCreateRoom {
+    width: 500px;
+    /* height: 400px; */
+    opacity: 0.9;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    margin-top: -200px;
+    margin-left: -250px;
+  }
 </style>
