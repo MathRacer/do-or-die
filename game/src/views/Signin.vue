7f578340a6c69c49b12458fb2905a08de08d7f34
harlesbayu <bayuanggaraharles@gmail.com>
2018-10-19 09:39:12 +0700

    interface game

---
+++ game/src/views/Signin.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="signin">
+    <div class="card text-center boxCreateRoom">
+      <div class="card-header" style="color: #f55052;">
+        MATH RACE
+      </div>
+      <div class="card-body">
+        <h5 class="card-title" style="color: #f55052;">CREATE USERNAME</h5>
+        <div class="form-group">
+          <input type="text" class="form-control" style="color: #f55052;" autofocus>
+        </div>
+        <a @click="btnSignin" class="btn btn-primary" style="background:#f55052; color: #ffffff; border: 1px #ffffff solid;"> <i class="fas fa-car"></i>
+          LETS GO !!!</a>
+      </div>
+      <div class="card-footer text-muted">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'signin',
+    components: {},
+    methods: {
+      btnSignin: function () {
+        this.$router.push("waitingroom")
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .signin {
+    background-image: url('../assets/signin.jpg');
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    min-width: 100%;
+    min-height: 100%;
+  }
+
+  .boxCreateRoom {
+    width: 500px;
+    /* height: 400px; */
+    opacity: 0.9;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    margin-top: -200px;
+    margin-left: -250px;
+  }
+</style>
---
+++ game/src/views/Waitiroom.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="waiting">
+    <div class="container boxCreateRoom">
+    <div class="row">
+      <div class="col-sm">
+        <div class="card" style="width: 19rem;">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <p class="card-text">
+              HARLES BAYU
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm">
+        <div class="card" style="width: 19rem;">
+          <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
+          <div class="card-body">
+            <p class="card-text">
+              HARLES BAYU
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button @click="btnWaitingRoom" style="width:640px; margin-top: 20px; background: #f55052; border: 1px #ffffff;" type="button" class="btn btn-primary btn-lg btn-block">START RACE</button>
+  </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'waitingroom',
+    components: {},
+    methods: {
+      btnWaitingRoom: function () {
+        // console.log('masuk')
+        this.$router.push("game")
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .waiting {
+    background-image: url('../assets/waitinglist.jpg');
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-size: cover;
+    min-width: 100%;
+    min-height: 100%;
+  }
+
+  .boxCreateRoom {
+    width: 670px;
+    /* height: 400px; */
+    opacity: 0.9;
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    margin-top: -180px;
+    margin-left: -350px;
+  }
+</style>
